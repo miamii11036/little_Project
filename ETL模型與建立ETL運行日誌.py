524f6be0fea7d8fd80547b2521359be1dffc4285
miamii11036 <lmiah7492@gmail.com>
2024-09-13 16:44:12 +0800

    main

--- ETL模型與建立ETL運行日誌.py
+++ ETL模型與建立ETL運行日誌.py
@@ -32,27 +32,30 @@ def extract_xml_file(filename):
         price = float(car.find('price').text)
         fuel = car.find('fuel').text
         xml_df = pd.concat(
-            [xml_df, pd.DataFrame([{'car_model':car_model,'year_of_manufacture':year_of_manufacture,'price':price,'fuel':fuel}])]
-            ,ignore_index=True
-        )
+            [xml_df,
+             pd.DataFrame([{'car_model':car_model,'year_of_manufacture':year_of_manufacture,'price':price,'fuel':fuel}])],
+             ignore_index=True)
     return xml_df
 
 #用glob搜尋對應的檔案格式，並套用剛剛創建的各檔案格式的資料提取function
 def extract():
     all_data_df = pd.DataFrame(columns=['car_model','year_of_manufacture','price','fuel'])
 
-    for csvfile in glob.glob('*.csv'):
+    for csvfile in glob.glob('*.csv'): #找到csv檔案並套用csv檔案資料提取的function
         all_data_df = pd.concat(
-            [all_data_df, pd.DataFrame(extract_csv_file(csvfile))],ignore_index = True
-        )
-    for jsonfile in glob.glob('*.json'):
+            [all_data_df, 
+             pd.DataFrame(extract_csv_file(csvfile))],
+             ignore_index = True)
+    for jsonfile in glob.glob('*.json'): #找到json檔案並套用json檔案資料提取的function
         all_data_df = pd.concat(
-            [all_data_df, pd.DataFrame(extract_json_file(jsonfile))],ignore_index=True
-        )
-    for xmlfile in glob.glob('*.xml'):
+            [all_data_df, 
+             pd.DataFrame(extract_json_file(jsonfile))],
+             ignore_index=True)
+    for xmlfile in glob.glob('*.xml'): #找到xml檔案並套用xml檔案資料提取的function
         all_data_df = pd.concat(
-            [all_data_df, pd.DataFrame(extract_xml_file(xmlfile))],ignore_index=True
-        )
+            [all_data_df, 
+             pd.DataFrame(extract_xml_file(xmlfile))],
+             ignore_index=True)
     return all_data_df
 
 #將價格的資料四捨五入取2位數，方便一眼辨識各car_model的價格
